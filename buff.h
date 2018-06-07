d4bd9e3de6c9144f6a87ac8e82d4d32eb4102b05
Kiki Lamb <miss.kiki.lamb@gmail.com>
2018-06-06 21:52:46 -0400

    ...

--- buff.h
+++ buff.h
@@ -1,3 +1,32 @@
+template <typename T> 
+class Buff256 {
+  private: 
+  uint8_t write_ix;
+  uint8_t read_ix;
+  uint8_t count;
+  T buff[256];
+  
+  public:
+  Buff256() : read_ix(0), write_ix(0), count(0) {}
+  ~Buff256() {}
+
+  inline void write(T t) {
+    buff[count++, write_ix++] = t;
+  }
+
+  inline T read() {
+    return buff[count--, read_ix++];
+  }
+
+  inline bool writeable() {
+    return count < 255;
+  }
+
+  inline bool readable() {
+    return count;
+  }
+};
+
 #define declare_buff256(t, name) volatile t name[256]; \
 volatile uint8_t name ## _write_ix = 0; \
 volatile uint8_t name ## _read_ix = 0; \
