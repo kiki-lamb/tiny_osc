37ca96901646625f73b4da9bd0096d5f2934623e
Kiki Lamb <miss.kiki.lamb@gmail.com>
2018-06-06 20:14:26 -0400

    ...

--- interfaces.h
+++ interfaces.h
@@ -18,7 +18,7 @@ class CustomSampleSource : public SampleSource<output_type_> {
   func_type func;
   CustomSampleSource(func_type f) : func(f) {};
   virtual ~CustomSampleSource() {};  
-  virtual output_type_ read() {
+  inline virtual output_type_ read() {
     return (*func)();
   }
 };
@@ -33,10 +33,15 @@ class SampleSink {
     SampleSource<input_type> * source;
       
     virtual ~SampleSink() {};
-
-    void connect(SampleSource<input_type> * source_ = NULL) {
+  
+    inline void connect(SampleSource<input_type> * source_ = NULL) {
       source = source_;
     }
+
+    protected:
+      inline SampleSource<input_type> * get_source() {
+        return source;
+      }
 };
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -51,12 +56,14 @@ class SampleProcessor : public SampleSource<output_type_> {
       
     virtual ~SampleProcessor() {};
 
-    void connect(SampleSource<input_type> * source_ = NULL) {
+    inline void connect(SampleSource<input_type> * source_ = NULL) {
       source = source_;
     }
-    virtual output_type read() {
+    
+    inline virtual output_type read() {
       return process(source->read());
     }
+    
     virtual output_type process(input_type) = 0;
 };
 
@@ -66,12 +73,16 @@ template <typename input_type_, typename output_type_ = input_type_ >
 class CustomSampleProcessor : public SampleProcessor<input_type_, output_type_> {
   public:
   typedef output_type_ (*func_type)(input_type_);
+  
   func_type func;
+  
   CustomSampleProcessor(func_type f, SampleSource<input_type_> * source = NULL) : func(f) {
     connect(source);
   };
+  
   virtual ~CustomSampleProcessor() {};
-  virtual output_type_ process(input_type_ v) {
+  
+  inline virtual output_type_ process(input_type_ v) {
     return (*func)(v);
   }
 };
