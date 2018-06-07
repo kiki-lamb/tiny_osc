bf86033982af1c84fb0d7682fe23b99a8e4451f4
Kiki Lamb <miss.kiki.lamb@gmail.com>
2018-06-07 00:38:55 -0400

    ...

---
+++ dsp_modules.h
@@ -0,0 +1,65 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <typename input, typename output = input>
+class Identity : public SampleProcessor<input, output> {
+  public:
+  virtual ~Identity() {};
+  Identity(SampleSource<input> * in = NULL) {
+    connect(in);
+  }
+  inline virtual output process(input v) {
+    return v;
+  }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class UnityMix : public SampleSource<int8_t> {
+  public:
+  Identity<int8_t> input1;
+  Identity<int8_t> input2;
+
+  virtual ~UnityMix() {}; 
+  
+  UnityMix(SampleSource<int8_t> * in1, SampleSource<int8_t> * in2) {
+      input1.connect(in1);
+      input2.connect(in2);
+  }
+
+  inline virtual int8_t read() {
+    uint16_t tmp;
+    tmp += input1.read();
+    tmp += input2.read();
+    return tmp / 2;
+  }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template <typename input>
+class ConvertToUnsigned : public SampleProcessor<input, typename sample_type_traits<input>::unsigned_type> {
+  public:
+  virtual ~ConvertToUnsigned() {};
+  ConvertToUnsigned(SampleSource<input> * in) {
+    connect(in);
+  }
+  inline virtual uint8_t process(input v) {
+    return sample_type_traits<input>::to_uint8_t(v);
+  }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template <typename input>
+class ConvertToSigned : public SampleProcessor<input, typename sample_type_traits<input>::signed_type> {
+  public:
+  virtual ~ConvertToSigned() {};
+  ConvertToSigned(SampleSource<input> * in) {
+    connect(in);
+  }
+  inline virtual int8_t process(input v) {
+    return sample_type_traits<input>::to_int8_t(v);
+  }
+};
+
+
+
