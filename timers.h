c70427af7937689f56ad42316aeebac7ba7f4b48
Kiki Lamb <miss.kiki.lamb@gmail.com>
2018-06-07 20:33:50 -0400

    Working on 328p.

--- timers.h
+++ timers.h
@@ -26,8 +26,8 @@ inline void setup_timers() {
 
   TCCR1B = (TCCR1B & ~_BV(WGM13)) | _BV(WGM12);
   TCCR1A = TCCR1A & ~(_BV(WGM11) | _BV(WGM10));
-  TCCR1B = (TCCR1B & ~(_BV(CS12))) | _BV(CS11) | _BV(CS10);  // Prescaler 64
-  OCR1A = 9; // 25KHz Samplefreq
+  TCCR1B = (TCCR1B & ~(_BV(CS11) | _BV(CS10))) | _BV(CS11) ;  // Prescaler 64
+  OCR1A = 79;   // 25000Hz Samplefreq (16000000/((79+1)*8))
   TIMSK1 |= _BV(OCIE1A);
   
   ASSR &= ~(_BV(EXCLK) | _BV(AS2));
