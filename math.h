12a2ad71708f84484df9ad347f94633ad8d33f95
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-04-26 23:04:28 -0400

    ...

--- math.h
+++ math.h
@@ -6,10 +6,10 @@ class Math {
     return x * y >> RSHIFT;  
   }
 
-  template <uint8_t Y, uint8_t RSHIFT = 8, typename T1>
-  static inline  __attribute__((always_inline)) T1 mul_U8S(T1 x) {
-    return x * Y >> RSHIFT;  
-  }
+//  template <uint8_t Y, uint8_t RSHIFT = 8, typename T1>
+//  static inline  __attribute__((always_inline)) T1 mul_U8S(T1 x) {
+//    return x * Y >> RSHIFT;  
+//  }
   
   private:
   Math();
